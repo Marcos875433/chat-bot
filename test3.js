cb637b50040f49e99b35ee406d676656deb9f332
Marcos875433 <marcosclashofclans29@gmail.com>
2021-08-21 23:15:57 -0500

    agregue test3

---
+++ test3.js
@@ -0,0 +1,8 @@
+var minuscula = ',reddit'
+var lulw = 'imagen'
+
+if(minuscula === ',reddit' + ' ' + lulw)
+
+if(minuscula === ',reddit') {
+    client.say(`especifica un reddit MAMA PINGA BabyRage`)
+}
