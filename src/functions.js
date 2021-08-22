30fe6288d8947d3baac1fb95e7daab5ea4f331ee
Marcos875433 <marcosclashofclans29@gmail.com>
2021-08-22 00:40:52 -0500

    subida completa

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export function findImage(strParameter, regeParameter, regeParameter2, idk, cliente) {
     for(let i = 0; i < 3; i++) {
         var randomImage = strParameter.match(regeParameter)
@@ -15,4 +16,23 @@ export function findImage(strParameter, regeParameter, regeParameter2, idk, clie
             return
         } 
     } 
+=======
+export function findImage(strParameter, regeParameter, regeParameter2, idk, cliente) {
+    for(let i = 0; i < 3; i++) {
+        var randomImage = strParameter.match(regeParameter)
+        var randomPost = strParameter.match(regeParameter2)
+        if (randomImage === null) {
+            if(i === 2) {
+                cliente.say(idk, `no encontre una PORONGA DE MONO BabyRage`)
+            }
+        } else {
+            for (let i = 0; i < 1; i++) {
+                var image = randomImage[i];
+                var post = randomPost[i];
+            }
+            cliente.say(idk, `${image} PogChamp, y aqui esta el tonto POST BabyRage https://www.reddit.com${post}`)
+            return
+        } 
+    } 
+>>>>>>> e5d4d4f08e36ed195e90f2a82b7868054ccef80d
 }
--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 require = require('esm')(module/*, options*/)
+=======
+require = require('esm')(module/*, options*/)
+>>>>>>> e5d4d4f08e36ed195e90f2a82b7868054ccef80d
 module.exports = require('./app')
