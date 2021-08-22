1e05a5d5bf75a229bcecf5dd4209ecf54fff50b6
Marcos875433 <marcosclashofclans29@gmail.com>
2021-08-22 01:01:19 -0500

    idk

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 require = require('esm')(module/*, options*/)
-=======
-require = require('esm')(module/*, options*/)
->>>>>>> e5d4d4f08e36ed195e90f2a82b7868054ccef80d
-=======
-require = require('esm')(module/*, options*/)
->>>>>>> e5d4d4f08e36ed195e90f2a82b7868054ccef80d
 module.exports = require('./app')
