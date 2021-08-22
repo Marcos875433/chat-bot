1957bbf71ead329b345ef9434e041f64be76f11a
Marcos875433 <58754180+Marcos875433@users.noreply.github.com>
2021-08-21 22:16:50 -0500

    Add files via upload

---
+++ src/functions.js
@@ -0,0 +1,18 @@
+export function findImage(strParameter, regeParameter, regeParameter2, idk, cliente) {
+    for(let i = 0; i < 3; i++) {
+        var randomImage = strParameter.match(regeParameter)
+        var randomPost = strParameter.match(regeParameter2)
+        if (randomImage === null) {
+            if(i === 2) {
+                cliente.say(idk, `no encontre una PORONGA DE MONO BabyRage`)
+            }
+        } else {
+            for (let i = 0; i < 1; i++) {
+                var image = randomImage[i];
+                var post = randomPost[i];
+            }
+            cliente.say(idk, `${image} PogChamp, y aqui esta el tonto POST BabyRage https://www.reddit.com${post}`)
+            return
+        } 
+    } 
+}
