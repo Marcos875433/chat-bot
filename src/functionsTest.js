230cc59c5be8479e9222286b0c982de6687a7fc8
Marcos875433 <marcosclashofclans29@gmail.com>
2021-08-22 00:40:52 -0500

    subida completa

--- src/functionsTest.js
+++ src/functionsTest.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export function testImage(strParameter, regeParameter, regeParameter2) {
     for(let i = 0; i < 3; i++) {
         var randomImage = strParameter.match(regeParameter)
@@ -16,3 +17,23 @@ export function testImage(strParameter, regeParameter, regeParameter2) {
         } 
     } 
 }
+=======
+export function testImage(strParameter, regeParameter, regeParameter2) {
+    for(let i = 0; i < 3; i++) {
+        var randomImage = strParameter.match(regeParameter)
+        var randomPost = strParameter.match(regeParameter2)
+        if (randomImage === null) {
+            if(i === 2) {
+                console.log('no encontre una PORONGA DE MONO BabyRage')
+            }
+        } else {
+            for (let i = 0; i < 1; i++) {
+                var image = randomImage[i];
+                var post = randomPost[i];
+            }
+            console.log(image + ' PogChamp, y aqui esta el tonto POST BabyRage https://www.reddit.com' + post)
+            return
+        } 
+    } 
+}
+>>>>>>> e5d4d4f08e36ed195e90f2a82b7868054ccef80d
