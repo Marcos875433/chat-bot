97bca682d3772feb49f56394e54291bf83d48a1e
Marcos875433 <marcosclashofclans29@gmail.com>
2021-08-24 00:30:05 -0500

    added some advises when not found random image

--- src/functions.js
+++ src/functions.js
@@ -1,18 +1,30 @@
+const request = require('request')
+
 export function findImage(strParameter, regeParameter, regeParameter2, idk, cliente) {
-    for(let i = 0; i < 3; i++) {
-        var randomImage = strParameter.match(regeParameter)
-        var randomPost = strParameter.match(regeParameter2)
-        if (randomImage === null) {
-            if(i === 2) {
-                cliente.say(idk, `no encontre una PORONGA DE MONO BabyRage`)
-            }
-        } else {
-            for (let i = 0; i < 1; i++) {
-                var image = randomImage[i];
-                var post = randomPost[i];
-            }
-            cliente.say(idk, `${image} PogChamp, y aqui esta el tonto POST BabyRage https://www.reddit.com${post}`)
-            return
-        } 
-    } 
+
+    var randomImage = strParameter.match(regeParameter)
+    var randomPost = strParameter.match(regeParameter2)
+    
+    if (randomImage === null) {
+        var regeBan = /\{\s+\"reason\"\:\s\"banned\"\,\s+\"message\"\:\s\"not\sfound\"\,\s+\"error\"\:\s404\s+\}/ig
+        var regeExist = /(\{\s+(\"kind\")\:\s(\"listing\")\,\s+(\"data\")\:\s\{\s+(\"after\")\:\s(null)\,\s+(\"dist\")\:\s(0)\,\s+(\"modhash\")\:\s(\"[\w\W]*\")\,\s+(\"geo\_filter\")\:\s(\"\")\,\s+(\"children\")\:\s(\[\])\,\s+(\"before\")\:\s(null)\s+\}\s+\})|(\{\s+\"message\"\:\s\"not\sfound\"\,\s+\"error\"\:\s404\s+\})/ig
+        var regePrivate = /\{\s+\"reason\"\:\s\"private\"\,\s+\"message\"\:\s\"forbidden\"\,\s+\"error\"\:\s403\s+\}/ig
+        if(randomPost !== null) {
+            cliente.say(idk, `no encontre ni una TONTA IMAGEN BabyRage intenta otra vez TONTO`)
+        } else if(strParameter.match(regeBan)) {
+            cliente.say(idk, `ese reddit esta BAN BabyRage`)
+        } else if(strParameter.match(regeExist)) {
+            cliente.say(idk, `ese reddit ni si quiera EXISTE BabyRage`)
+        } else if(strParameter.match(regePrivate)) {
+            cliente.say(idk, `ese reddit es PRIVADISIMO BabyRage`)
+        }
+    } else {
+        for (let i = 0; i < 1; i++) {
+            var image = randomImage[i];
+            var post = randomPost[i];
+        }
+        cliente.say(idk, `${image} PogChamp, y aqui esta el tonto POST BabyRage https://www.reddit.com${post}`)
+        return
+    }
+
 }
