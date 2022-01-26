da3910a4f6f035d6f834bb9f0b13f71a61023b46
Marcos875433 <marcosclashofclans29@gmail.com>
2022-01-26 17:53:02 -0600

    added "streamed", "echo", and "myfollows" commands

--- src/app.js
+++ src/app.js
@@ -2,14 +2,20 @@ require('dotenv').config();
 
 const tmi = require('tmi.js');
 const request = require('request');
-const fetch = require('node-fetch')
-const fs = require('fs')
+const fetch = require('node-fetch');
+const fs = require('fs');
+const admin = require('firebase-admin');
+const split = require('shlex').split;
 
-import { findImage, top, addChannel, getSub } from './functions';
+import { findImage, top, getSub, fireConfig, checkList, checkChannel, checkConnection, doPastebinShit, getCatsPlayed, getFollows, getBotID } from './functions';
+
+(async() => {
 
 const botUserName = process.env.botUserName;
 const ouathToken = process.env.ouathToken;
-var channelNames = ["#srsarcasmo01","#darkness_lalo874","clark_eit"]
+fireConfig();
+const db = admin.database();
+var channelNames = await checkList(db);
 
 const options = {
     options: {
@@ -28,7 +34,7 @@ const client = new tmi.client(options)
 client.connect();
 
 client.on('connected', (adress, port) => {
-    console.log('a')
+    console.log('a');
 });
 
 client.on("join", (channel, username) => {
@@ -39,26 +45,20 @@ client.on("join", (channel, username) => {
 
 var saBlock = false;
 
-client.on('chat', (target, ctx, message, self) => {
-    if (self) return;
+client.on('chat', async(target, ctx, message, self) => {
+    // if (self) return;
 
     const spaces = message.trim();
     const minuscula = spaces.toLowerCase();
     var subredditRegex = /(?<=\,reddit\s+)(\w+)/ig
     var topRegex = /(?<=\,top\s+)(\w+)/ig
-    var checkUser = '#' + ctx.username
+    // var checkUser = '#' + ctx.username
 
-    var isInList = channelNames.includes(checkUser); // channelNames.indexOf(checkUser) !== -1;
+    // var isInList = channelNames.includes(checkUser); // channelNames.indexOf(checkUser) !== -1;
 
     console.log(ctx.username)
     if(target === '#srsarcasmo01' && minuscula === ',join') {
-        if (!isInList) {
-            channelNames.push(ctx.username)
-            var newArray = JSON.stringify(channelNames)
-            addChannel(newArray, client, target)
-        } else {
-           client.say(target, `eres un MAMA HUEVAZO BabyRage, ya esta el bot unido a tu chat`) 
-        }
+        await checkChannel(db, '#' + ctx.username, client, target, channelNames);
     }
     
     if(minuscula.match(subredditRegex)) {
@@ -119,7 +119,7 @@ client.on('chat', (target, ctx, message, self) => {
         client.say(target, `COMOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO BabyRage`)
     }
 
-    if(minuscula.match(/^\,sa/i)) { // if the start of the comment match with ",sa" do this
+    if(minuscula.match(/^\,sa(?![\w\d])/i)) { // if the start of the comment match with ",sa" do this
         if (!saBlock) { // if sa is not blocked/timed out
             var sa = minuscula.split(' ')
             if (sa.length > 3) {
@@ -140,4 +140,60 @@ client.on('chat', (target, ctx, message, self) => {
             }, 5000);
         }
     }
+
+    if(minuscula.match(/^\,streamed(?![\w\d])/i)) { // ,streamed - [streamer] [game] || ["followed"] [game]
+        let streamed = split(minuscula);
+        if (streamed.length === 3) {
+            if (streamed[1] === 'followed') {
+                const daUserID = self ? await getBotID(botUserName) : ctx['user-id'];
+                const daResponse = await getCatsPlayed(true, streamed[2], {user_id: daUserID});
+                if (!daResponse) {
+                    client.say(target, `Algo salio mal FeelsDankMan`);
+                } else {
+                    const daStreamers = daResponse[0].join(', ');
+                    const daAmountOfStreamers = daResponse[1];
+                    if(daStreamers.length > 500) {
+                        const daPaste = await doPastebinShit(daStreamers);
+                        client.say(target, `Aqui esta la lista de streamers que han jugado ${streamed[2]} -> ${daPaste} , son ${daAmountOfStreamers} streamers PogChamp`);
+                    } else {
+                        client.say(target, `${daStreamers}`);
+                    }
+                }
+            } else {
+                const daResponse = await getCatsPlayed(false, streamed[2], {daChannel: streamed[1]});
+                if (!daResponse) {
+                    client.say(target, `Algo salio mal FeelsDankMan`);
+                } else if (daResponse === 'STREAMER') {
+                    client.say(target, `Si, tu streamer ha stremeado ${streamed[2]}`);
+                } else if (daResponse === `No Streamer Sadge`) {
+                    client.say(target, `Tu Streamer no ha stremeado esa PORONGA KEKW`);
+                }
+            }
+        }
+    }
+
+    if (minuscula === ',channels') {
+        console.log(channelNames)
+    }
+
+    if (minuscula.match(/^\,echo(?![\w\d])/i) && ctx.username === 'srsarcasmo01') {
+        const daEcho = minuscula.split(' ')
+        daEcho.shift();
+        client.say(target, daEcho.join(' '));
+    }
+
+    if (minuscula.match(/^\,myfollows(?![\w\d])/i)) {
+        const daBotID = await getBotID(botUserName);
+        const myFollows = await getFollows(self ? daBotID : ctx['user-id']);
+        let deFollows = myFollows[0].join(', ');
+        const daAmountOfFollows = myFollows[1];
+
+        if (deFollows.length > 500) {
+            const daPaste = await doPastebinShit(deFollows);
+            client.say(target, `aqui esta tu lista de follows ${daPaste} con ${daAmountOfFollows} follows`);
+        } else {
+            client.say(target, `${deFollows}`);
+        }
+    }
 });
+})();
