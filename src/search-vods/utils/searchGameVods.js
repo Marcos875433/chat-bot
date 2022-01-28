be1e0f0e98ba9c7552d6a940db106aad5a7a10c2
Marcos875433 <marcosclashofclans29@gmail.com>
2022-01-28 00:42:50 -0600

    client_id, loops
    changed oauthToken to a client_id, changed forEach loops to for of loops, and fixed extra loops I guess

--- src/search-vods/utils/searchGameVods.js
+++ src/search-vods/utils/searchGameVods.js
@@ -2,14 +2,14 @@ const getVodsMoments = require("./getVodsMoments");
 const millisecondsToHms = require("./millisecondsToHms");
 const getVods = require("./getVods");
 
-async function searchGameVods(channel, token, game) {
+async function searchGameVods(channel, client_id, game) {
     const response = [];
 
-    const vods = await getVods(channel, token);
+    const vods = await getVods(channel, client_id);
     if (vods === 'The channel has no vods') return;
-    const moments = await getVodsMoments(token, vods);
+    const moments = await getVodsMoments(client_id, vods);
 
-    vods.forEach((vod) => {
+    for (let vod of vods) {
         // si nunca se cambio la categoria del stream, no aparece ningun momento
         // entonces vamos a sacar el juego de la info del vod en lugar de los momentos
         // con esto tambien conseguimos todos los primeros juegos de los vods
@@ -18,30 +18,32 @@ async function searchGameVods(channel, token, game) {
         /*const id = vod.node.id;
         const url = `https://www.twitch.tv/videos/${id}`;*/
 
-        if (!vodGame) return; // no tiene juego
+        if (!vodGame) continue; // no tiene juego
 
         if (vodGame.displayName.toLowerCase() == game.toLowerCase()) {
             if (!response[0]) { // if the channel has not been pushed
                 response.push(channel); // push(url)
+                return response;
             }
         }
-    });
+    }
 
-    moments.forEach((moment) => {
+    for (let moment of moments) {
         const momentGame = moment.node.details.game;
         const milliseconds = moment.node.positionMilliseconds;
         /*const timestamp = millisecondsToHms(milliseconds);
         const id = moment.node.video.id;
         const url = `https://www.twitch.tv/videos/${id}?t=${timestamp}`;*/
 
-        if (milliseconds == 0) return; // ignoramos el primer juego por lo de antes
-        if (!momentGame) return; // no tiene juego
+        if (milliseconds == 0) continue; // ignoramos el primer juego por lo de antes
+        if (!momentGame) continue; // no tiene juego
         if (momentGame.displayName.toLowerCase() == game.toLowerCase()) {
             if (!response[0]) { // if the channel has not been pushed
                 response.push(channel); // push(url)
+                return response;
             }
         }
-    });
+    }
 
     return response;
 }
